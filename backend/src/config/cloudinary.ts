308f88e8219e9ff8d07210c876bb40836a345afb
Solomon getnet <Solomongetnetofficial@gmail.com>
2024-07-20 03:43:03 +0300

    cloduinary env

--- backend/src/config/cloudinary.ts
+++ backend/src/config/cloudinary.ts
@@ -2,10 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 
 // Configuration
 cloudinary.config({
-  cloud_name: "djwb017ip" || process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: "466366784735428" || process.env.CLOUDINARY_API_KEY,
-  api_secret:
-    "0DM3i4R88NqDoYOrNn3rkkm8DDE" || process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
